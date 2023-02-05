9c1ff809036c937d1208ac59c3d85f697b7c76ec
Konstantin Ivanov <konstantin.ivanov.nf@gmail.com>
2023-02-05 14:27:15 +0200

    Loodud esmane komponendi disain.

---
+++ PrjTeisendajaKomponent/UiWindowClassFile.vb
@@ -0,0 +1,9 @@
+﻿Public Class UiWindowClassFile
+    Implements UiWindowClassFileInterface
+
+    Const KM2MI = 1.609344
+
+    Public Function convertKilometrsToMiles(ByRef kilometers As Double) As Double Implements UiWindowClassFileInterface.convertKilometrsToMiles
+        Return kilometers / KM2MI
+    End Function
+End Class
